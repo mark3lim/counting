675cced2007d3eabcd337ca81b99bd86cd94883c
mark3lim <xorbs228@gmail.com>
2025-12-05 13:00:19 +0900

    Initial Commit

---
+++ counting/countingApp.swift
@@ -0,0 +1,17 @@
+//
+//  countingApp.swift
+//  counting
+//
+//  Created by MARKLIM on 2025-12-05.
+//
+
+import SwiftUI
+
+@main
+struct countingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
