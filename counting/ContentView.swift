4fd1f46722f5b46feda04d9bb9c8eed6dc714621
mark3lim <xorbs228@gmail.com>
2025-12-05 13:16:34 +0900

    first commit

--- counting/ContentView.swift
+++ counting/ContentView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "message")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
+                .foregroundStyle(.green)
             Text("Hello, world!")
         }
         .padding()
