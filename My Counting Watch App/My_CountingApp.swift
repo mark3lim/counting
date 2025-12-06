954d820c62749c694d5366788d373853afa28a19
mark3lim <xorbs228@gmail.com>
2025-12-07 01:19:59 +0900

    Add initial Watch App target with basic UI and tests

---
+++ My Counting Watch App/My_CountingApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_CountingApp.swift
+//  My Counting Watch App
+//
+//  Created by MARKLIM on 2025-12-07.
+//
+
+import SwiftUI
+
+@main
+struct My_Counting_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ My Counting Watch AppTests/My_Counting_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  My_Counting_Watch_AppTests.swift
+//  My Counting Watch AppTests
+//
+//  Created by MARKLIM on 2025-12-07.
+//
+
+import Testing
+@testable import My_Counting_Watch_App
+
+struct My_Counting_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
